83dcdbc6b90ae52c4e8e3f4fb9a577388f4c500c
MAVIRE CODOIR Git <administrator@mavirecodoir.com>
2025-08-09 01:41:39 +0100

    Update ClaimPortal.js

--- src/components/ClaimPortal.js
+++ src/components/ClaimPortal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './ClaimPortal.css';
+import './ClaimPortal.js';
 
 const ClaimPortal = () => {
   const [claimInput, setClaimInput] = useState('');
